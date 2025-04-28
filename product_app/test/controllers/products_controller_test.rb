f24b3f9a2de789cc90cd46ca1590b98262276f29
jkinlan <jkinlan@kravemedia.ie>
2025-04-28 22:29:43 +0100

    Implement unit tests for the model and controller actions

--- product_app/test/controllers/products_controller_test.rb
+++ product_app/test/controllers/products_controller_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @product = Product.create(name: "Test Product", description: "Test Description", price: 9.99)
+  end
+
+  test "should get index" do
+    get products_url
+    assert_response :success
+  end
+
+  test "should create product" do
+    assert_difference('Product.count') do
+      post products_url, params: { product: { name: 'New Product', price: 12.99, description: 'Something' } }
+    end
+    assert_response :created
+  end
+
+  test "should show product" do
+    get product_url(@product)
+    assert_response :success
+  end
+
+  test "should update product" do
+    patch product_url(@product), params: { product: { price: 15.99 } }
+    assert_response :success
+  end
+
+  test "should destroy product" do
+    assert_difference('Product.count', -1) do
+      delete product_url(@product)
+    end
+    assert_response :no_content
+  end
 end
--- product_app/test/models/product_test.rb
+++ product_app/test/models/product_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save product without name" do
+    product = Product.new(price: 10.0)
+    assert_not product.save, "Saved the product without a name"
+  end
+
+  test "should not save product with negative price" do
+    product = Product.new(name: "Test", price: -1)
+    assert_not product.save, "Saved product with negative price"
+  end
 end
